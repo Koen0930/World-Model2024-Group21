80cab06ea1455273de88eaa1e07d945b2e529257
Koen0930 <koen-yusei851@g.ecc.u-tokyo.ac.jp>
2024-12-26 23:05:57 +0900

    update: logdir

--- scripts/run_montezuma.sh
+++ scripts/run_montezuma.sh
@@ -13,7 +13,7 @@ shift
 export CUDA_VISIBLE_DEVICES=$device; python dynalang/train.py \
   --run.script train \
   --run.log_keys_video log_image \
-  --logdir ~/logdir/atari/${task}_${name} \
+  --logdir /workspace/logdir/atari/${task}_${name} \
   --use_wandb False \
   --task gym_ALE/MontezumaRevenge-v5 \
   --envs.amount 16 \

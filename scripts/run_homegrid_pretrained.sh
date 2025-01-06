3bb9d763b6f88d1b8088afcb9126dd3902a81d58
Koen0930 <koen-yusei851@g.ecc.u-tokyo.ac.jp>
2025-01-06 05:07:08 +0000

    add: fine-tuning script

---
+++ scripts/run_homegrid_pretrained.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+task=$1
+name=$2
+device=$3
+seed=$4
+
+shift
+shift
+shift
+shift
+
+export CUDA_VISIBLE_DEVICES=$device; python dynalang/train.py \
+  --run.script train \
+  --logdir logdir/homegrid/$name \
+  --use_wandb True \
+  --task $task \
+  --envs.amount 1 \
+  --seed $seed \
+  --encoder.mlp_keys token$ \
+  --decoder.mlp_keys token$ \
+  --decoder.vector_dist onehot \
+  --batch_size 16 \
+  --batch_length 256 \
+  --run.train_ratio 32 \
+  --load_wm_ckpt True \
+  --run.from_checkpoint /home/ubuntu/World-Model2024-Group21/logdir/textpt/exp1_42/checkpoint.pkl \
+  "$@"
